667d35c5cd8dafafeaee7b0434fced34f4fb8d74
Hackertux <gatetefanta@outlook.com>
2024-11-24 11:44:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,7 @@
 echo "Instalando Scapy..."
 pip install scapy
 
-# Clonar el repositorio desde GitHub
-echo "Clonando el repositorio..."
-git clone https://github.com/Hackertux/proyecto-raspberry-pi-3-hacking.git
-
 # Navegar al directorio del proyecto y ejecutar el script
 echo "Ejecutando el script proyecto.py..."
-cd proyecto-raspberry-pi-3-hacking/proyecto || exit
+cd raspihack-V1/proyecto || exit
 python3 proyecto.py
